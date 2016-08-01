7184a28c071f50faa841b808423eac915d6165ae
Garrett Heaver <garrett@iterationfour.com>
2016-08-01 01:08:25 +0100

    updated entrypoint to deal better with restarts

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,19 +2,31 @@
 
 # check args
 if [ "$#" -ne 2 ]; then
-  echo "usage: $0 <repository> <executable>"
+  echo "usage: $0 <repo> <script>"
   exit 1
 fi
 
-# check if git needs ssh
-if [ ${1:0:4} = "git@" ]; then
-  apk add openssh-client && mkdir ~/.ssh
-  ssh-keyscan -H github.com >> ~/.ssh/known_hosts
-  cat /etc/ssh/config >> ~/.ssh/config
+# initial container startup
+if [ ! -d "/opt/.git" ]; then
+
+  # check if git needs ssh
+  if [ ${1:0:4} = "git@" ]; then
+    apk add openssh-client && mkdir -p ~/.ssh
+    ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+    cat /etc/ssh/config >> ~/.ssh/config
+  fi
+
+  # first clone
+  git clone "$1" /opt
+
+else
+
+  # simple update required
+  $(cd /opt && git pull origin)
+
 fi
 
 # bootstrap container
-git clone "$1" /opt && \
-  chmod +x "/opt/$2" && \
+chmod +x "/opt/$2" && \
   eval "/opt/$2"
 
