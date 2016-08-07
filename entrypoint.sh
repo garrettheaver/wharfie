21a321f870eaf84b05646967d6994ce05ebabf49
Garrett Heaver <garrett@iterationfour.com>
2016-08-07 22:14:41 +0100

    removed previous superfluous logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,13 @@
 #! /bin/sh
 
+# log out the start time
+echo "entrypoint: `date -u \"+%Y-%m-%d %H:%M:%S\"` UTC"
+
 # check correct args
 if [ "$#" -ne 2 ]; then
-  echo "usage: <source> <script>"
+  echo "usage: ./entrypoint.sh <source> <script>"
   exit 1
 fi
-
-# blank log line between starts
-if [ -d "/opt/.git" ]; then
-  printf "\n"
-fi
-
-# log out the start time of the container
-echo "entrypoint: `date -u \"+%Y-%m-%d %H:%M:%S\"` UTC"
 
 # initial container startup
 if [ ! -d "/opt/.git" ]; then
@@ -25,19 +20,16 @@ if [ ! -d "/opt/.git" ]; then
   fi
 
   # first clone
-  echo "git: clone $1"
   git clone "$1" /opt
 
 else
 
-  # simple update required
-  echo "git: pull `git -C /opt config --get remote.origin.url`"
+  # only pull required
   git -C /opt pull origin
 
 fi
 
 # bootstrap container
-echo "entrypoint: exec $2"
 chmod +x "/opt/$2" && \
   eval "/opt/$2"
 
