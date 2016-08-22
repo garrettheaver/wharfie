ffb4e7d9cc1fda5855dcb28dfce04ff91f68f31a
Garrett Heaver <garrett@iterationfour.com>
2016-08-22 23:13:43 +0100

    added some comments about the armhf script

--- armhf.sh
+++ armhf.sh
@@ -1,5 +1,10 @@
 #! /bin/sh
 
+# This script is used to build the armhf variant of Wharfie and uses the
+# appropriate version of the armhf-alpine container as a base. It must be
+# executed on an arm device with a running Docker service which has already
+# been authenticated to Docker Hub via `docker login`
+
 # line up the files
 cp Dockerfile dockerfile.x86
 sed -i -e 's/FROM alpine:/FROM container4armhf\/armhf-alpine:/g' Dockerfile
