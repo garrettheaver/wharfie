89645ec53ae9d86a3a086ee59f3d26a5279eb771
Garrett Heaver <garrett@iterationfour.com>
2016-08-26 21:07:19 +0100

    simplified the armhf Docker build script

--- armhf.sh
+++ armhf.sh
@@ -5,14 +5,13 @@
 # executed on an arm device with a running Docker service which has already
 # been authenticated to Docker Hub via `docker login`
 
+REPO=${1:-garrettheaver/armhf-wharfie}
+BASE=${2:-container4armhf/armhf-alpine}
+
 # line up the files
-cp Dockerfile dockerfile.x86
-sed -i -e 's/FROM alpine:/FROM container4armhf\/armhf-alpine:/g' Dockerfile
+sed -e 's|FROM .*:|FROM '"$BASE"':|g' Dockerfile > Dockerfile.arm
 
-# trigger the build and push
-docker build --rm --tag garrettheaver/armhf-wharfie .
-docker push garrettheaver/armhf-wharfie
-
-# cleanup
-mv dockerfile.x86 Dockerfile
+# trigger the build
+docker build --rm --tag="$REPO" --file="Dockerfile.arm" .
+docker push "$REPO"
 
